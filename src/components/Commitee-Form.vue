888a123541624ce2c8021ebbc7f6f12796d5a353
AhmadQadouraCubes <ahmad.qadourah@cubesplatform.com>
2023-08-21 15:41:03 +0300

    + form

---
+++ src/components/Commitee-Form.vue
@@ -0,0 +1,47 @@
+<template>
+  
+    <h2>Add Consultant</h2>
+    <div class="d-flex flex-column mt-3">
+      <span>Name<span class="text-danger">*</span></span>
+      <input type="text" />
+    </div>
+    <div class="d-flex flex-column mt-3">
+      <span>Entity Name<span class="text-danger">*</span></span>
+      <input type="text" />
+    </div>
+    <div class="d-flex flex-column mt-3">
+      <span>Occupation<span class="text-danger">*</span></span>
+      <input type="text" />
+    </div>
+    <div class="d-flex justify-content-start">
+      <div class="d-flex flex-column mt-3 w-100">
+        <span>start Date<span class="text-danger">*</span></span>
+        <input type="text" class="ps-5 pe-5 me-4" />
+      </div>
+      <div class="d-flex flex-column mt-3 w-100">
+        <span>end date<span class="text-danger">*</span></span>
+        <input type="text" class="ps-5 pe-5" />
+      </div>
+  </div>
+  <div class="d-flex">
+    
+    <p><input type="checkbox" class="me-2" />Consulting services are still ongoing</p>
+  </div>
+<div>
+    <p>Reasons for hiring <span class="badge bg-secondary">Optional</span></p> 
+<textarea name="ReasonsForHiring" id="1" cols="119" rows="2"></textarea>
+</div>
+<div>
+    <p>Achievements <span class="badge bg-secondary">Optional</span></p> 
+<textarea name="ReasonsForHiring" id="1" cols="119" rows="2"></textarea>
+</div>
+
+<div class="d-flex flex-row-reverse bd-highlight">
+    <button class="ps-2 pe-1 btn btn-primary" style="background-color:#b88c33 ; font-size: smaller; ">Add Consultant</button>
+    <button class="btn btn-light me-3">Cancel</button>
+</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/components/Commitee-members.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="p-3">
+    <h5 class="">Committee-Members</h5>
+    <div class="pt-1 ps-2 d-flex flex-column">
+      <div>
+        <span> Internal Committee Members</span>
+        <span class="text-danger">*</span>
+      </div>
+      <div class="d-flex flex-row mt-1">
+        <input
+          type="text"
+          class="border border-black border border-2 rounded w-100 me-3 p-1"
+        />
+        <button class="addButton4 ps-5 pe-5 btn btn-primary">Add</button>
+      </div>
+
+      <div class="mt-5">
+        <table class="table ">
+          <thead class="table-light">
+            <tr>
+              <th scope="col"><input type="checkbox" /></th>
+              <th scope="col">Name</th>
+              <th scope="col">Administrative dependency</th>
+              <th scope="col">Entity</th>
+              <th scope="col">E-mail</th>
+              <th scope="col">Phone Number</th>
+            </tr>
+          </thead>
+          <!-- <img :src=pic alt="pic" class="rounded-circle" style="width: 2vw;"> -->
+          <tbody> 
+            
+            <tr>
+              <th scope="row"><input type="checkbox" /></th>
+              <td>Ahmad</td>
+              <td>Manager</td>
+              <td>Ministry of energy</td>
+              <td>ahmad@gmail.com</td>
+              <td>078888888</td>
+              <td scope="col">
+                <img
+                  :src="trash"
+                  alt="trash"
+                  class="border border-3 rounded-circle p-1"
+                />
+              </td>
+
+              <!-- <div class="d-flex flex-column">
+                  <p>Area of expertise , skills and experience</p>
+                  <textarea></textarea>
+                </div> -->
+            </tr>
+
+            <tr>
+              <th scope="row"><input type="checkbox" /></th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row"><input type="checkbox" /></th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import trash from "../assets/trash-fill.svg";
+import pic from "../assets/pic.jpg";
+
+const openForm=()=>{
+
+}
+</script>
+
+<style scoped>
+.addButton4 {
+  background-color: #b88c33;
+}
+</style>
